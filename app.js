00167f233062b3bbdb913fceedaff5eb24f943f6
Erik Shandrowski <erik843@gmail.com>
2014-07-29 15:53:33 -0600

    Finished form

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// beglobal api key ZrlNkRgRkQke3sbr7hDUIQ%3D%3D
+// curl -X POST -H "Content-type: application/json" -H "Authorization: LC apiKey=<YOUR API KEY>" -d '{"text":"Hello Developers", "from":"eng", "to":"fra"}' https://lc-api.sdl.com/translate
 var express = require('express');
 var bodyParser = require('body-parser');
+var BeGlobal = require('node-beglobal');
+
+var beglobal = new BeGlobal.BeglobalAPI( {
+	api_token: 'ZrINkRgRkQke3sbr7hDUlQ%3D%3D'
+});
 
 var app = express();
 app.set('view engine', 'jade');
@@ -9,6 +16,20 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.post('/translate', function(req, res) {
+	beglobal.translations.translate(
+	  {text: req.body.text, from: req.body.from, to: req.body.to},
+	  function(err, results) {
+	    if (err) {
+	      return console.log(err);
+	    }
+
+	    console.log(results);
+	    res.render('index', {
+	    	translation: results})
+	  });
 });
 
 var server = app.listen(3157, function() {
